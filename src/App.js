e3f111be836d6b43760852cb6e2e45094b948f24
PauloEmanuel <smariaclarice8@gmail.com>
2025-02-26 09:26:47 -0300

    retirando e Atualizado o App.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const name= "Batman";
+  let a = 1;
+  let b = 2;
+
+  function sum(a, b){
+  
+    return a +b;
+
+  }
+
+const url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUSU1AUL-ZTT8FTkMiSfQGaPvZN9MQkDLnNA&s";
+
+return(
+  <div className='App'>
+    <p>Testando o Jsx</p>
+    <p>Olá {name}</p>
+    <p>Soma é {sum(1,2)}</p>
+    <img src='{url}'alt='informação da imagem'></img>
+  </div>
+)
+
 }
 
 export default App;
